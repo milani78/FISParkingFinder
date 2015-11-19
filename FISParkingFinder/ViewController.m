2358840d170b3f96470fa1a634e811a42b1b806b
mattchang <msc249@gmail.com>
2015-11-19 10:41:26 -0500

    Cleaned things up.  Not trying to make circles tappable anymore

--- FISParkingFinder/ViewController.m
+++ FISParkingFinder/ViewController.m
@@ -45,8 +45,8 @@
     MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(40.707721, -74.012952), .8*METERS_PER_MILE,.8*METERS_PER_MILE);
     [self.mapView setRegion:viewRegion animated:YES];
     [self.mapView regionThatFits:viewRegion];
-    
-    [self.mapView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mapTapped:)]];
+    //commented out because tapping the map caused a crash
+//    [self.mapView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mapTapped:)]];
     
     
     /*****************************
@@ -59,7 +59,8 @@
                       pathForResource:@"manhattan-sign-locations" ofType:@"sqlite"];
     NSLog(@"DATABASE IS:%@", sqlitePath);
     
-    if (![[NSFileManager defaultManager] fileExistsAtPath:sqlitePath]) {
+    if (![[NSFileManager defaultManager] fileExistsAtPath:sqlitePath])
+    {
         NSLog(@"DID NOT FIND FILE AT %@", sqlitePath);
     }
 
@@ -67,7 +68,8 @@
     // open the database
     
     sqlite3 *theDatabase;
-    if (!(sqlite3_open([sqlitePath UTF8String], &theDatabase) == SQLITE_OK)) {
+    if (!(sqlite3_open([sqlitePath UTF8String], &theDatabase) == SQLITE_OK))
+    {
         NSLog(@"ERROR. COULD NOT OPEN AT %@", sqlitePath);
     }
     
@@ -85,7 +87,8 @@
     
     sqlite3_stmt *sqlStatement;
     int status = sqlite3_prepare(theDatabase, sql, -1, &sqlStatement, NULL);
-    if (status != SQLITE_OK) {
+    if (status != SQLITE_OK)
+    {
         NSLog(@"PROBLEM WITH PREPARE STATEMENT: %d %s", status, sqlite3_errmsg(theDatabase));
     }
     
@@ -94,7 +97,8 @@
     
     self.signObjects = [[NSMutableArray alloc] init];
 
-    while (sqlite3_step(sqlStatement) == SQLITE_ROW) {
+    while (sqlite3_step(sqlStatement) == SQLITE_ROW)
+    {
         NSInteger signSequence = (NSInteger)sqlite3_column_int(sqlStatement, 2);
         NSInteger signFeetFromStreet = (NSInteger)sqlite3_column_int(sqlStatement, 3);
         char *signRegulation = (char *)sqlite3_column_text(sqlStatement, 5) ?: "";  //  ?: ""   is for when the entry is null, to feed an empty string
@@ -125,38 +129,9 @@
     
     NSLog(@"%@", self.signObjects);
 
- 
-    
-    
-    
-    /*****************************
-     *       TRYING FISSign      *
-     *****************************/
-    
-    // from Date Picker
-    
-//     NSDate *currentDate = self.datePicker.date;
-//     NSCalendar *currentCalendar = [NSCalendar currentCalendar];
-//     NSDateComponents *dateComponents = [currentCalendar components:(NSCalendarUnitWeekday | NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:currentDate];
-//     NSUInteger dayOfWeek = [dateComponents weekday];
-//     NSUInteger hour = [dateComponents hour];
-//    NSLog(@"%lu", hour);
-//    // placeholder days array
-//    
-//     NSArray *daysOfTheWeek = @[@"nil", @"Sunday", @"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday"];
-    
-
-    //putting sign objects on the map
-    
-//commented out for time being to test IF STATEMENT
-    //    FISSign *aSign = [[FISSign alloc] initWithCoordinates:CLLocationCoordinate2DMake(40.707721, -74.012952) hourStarts:12 hourEnds:13 signDays:daysOfTheWeek regulation:@"FREE PARKING ALL THE TIME! :)"withMapView:self.mapView withDatePicker:self.datePicker];
-//    
-//
-//    FISSign *anotherSign = [[FISSign alloc] initWithCoordinates:CLLocationCoordinate2DMake(40.707019, -74.013433) hourStarts:13 hourEnds:15 signDays:daysOfTheWeek regulation:@"FREE PARKING ALL THE TIME! :)"withMapView:self.mapView withDatePicker:self.datePicker];
-
 
     
-    }
+}
 
 //Moved the picker to give viewController data from it for IF STATEMENT
 - (IBAction)datePicker:(id)sender
@@ -197,7 +172,8 @@
 
     // this removes ALL overlays
     [self.mapView removeOverlays: self.mapView.overlays];
-    for (FISSign *sign in dots) {
+    for (FISSign *sign in dots)
+    {
         for (NSUInteger i = 0; i < (sign.signDays.count); i++)
         {
             NSLog(@"in for loop in datePickerDidPickDate");
@@ -221,7 +197,8 @@
 
     NSLog(@"GETTING CALLED MAP VIEW THING\n\n\n\n\n HI HI HI HI\n\n\n\n\n");
     
-    if ([overlay isKindOfClass:FISCircle.class]) {
+    if ([overlay isKindOfClass:FISCircle.class])
+    {
         
         FISCircle *circle = (FISCircle *)overlay;
         UIColor *colorOfCircle = circle.color;
@@ -234,172 +211,5 @@
     
     return nil;
 }
-//making circles tappable
-
--(void)mapTapped:(UITapGestureRecognizer *)recognizer
-{
-    
-    CGPoint point = [recognizer locationInView:self.mapView];
-    
-    NSArray *arrayOfStuff = self.mapView.overlays;
-
-    
-    id<MKOverlay> tappedOverlay = nil;
-    for (FISCircle *circle in self.mapView.overlays)
-    {
-        
-        MKOverlayRenderer * polygonRenderer = [self.mapView rendererForOverlay:circle];
-        if ( [polygonRenderer isKindOfClass:[MKPolygonRenderer class]]) {
-            
-            //Convert the point
-            CLLocationCoordinate2D  coordinate = [self.mapView convertPoint:point
-                                                       toCoordinateFromView:self.mapView];
-            MKMapPoint mapPoint = MKMapPointForCoordinate(coordinate);
-            CGPoint polygonViewPoint = [polygonRenderer pointForMapPoint:mapPoint];
-            
-            // with iOS 7 you need to invalidate the path, this is not required for iOS 8
-            
-            
-            
-            
-//            tapInPolygon = CGPathContainsPoint(polygonRenderer.path, NULL, polygonViewPoint, NO);
-        }
-        
-        
-        
-        
-//        MKPolygonView *view = (MKPolygonView *)[self.mapView viewForOverlay:circle];
-//        
-//        if (view){
-//            CGPoint touchPoint = [recognizer locationInView:self.mapView];
-//            CLLocationCoordinate2D touchMapCoordinate =
-//            [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
-//            
-//            MKMapPoint mapPoint = MKMapPointForCoordinate(touchMapCoordinate);
-//            
-//            CGPoint polygonViewPoint = [view pointForMapPoint:mapPoint];
-//            if(CGPathContainsPoint(view.path, NULL, polygonViewPoint, NO)){
-//                tappedOverlay = view;
-//                tappedOverlay.tag = i;
-//                break;
-//            }
-
-
-        
-        
-        CGRect rectOfCircle = CGRectMake(circle.boundingMapRect.origin.x, circle.boundingMapRect.origin.y, circle.boundingMapRect.size.width, circle.boundingMapRect.size.height);
-        
-        
-        CGRect something = [self.view convertRect:rectOfCircle fromView:self.mapView];
-        
-        
-        
-//        CGPoint test = [self.view convertRect: fromView:<#(nullable UIView *)#>]
-        
-        MKOverlayRenderer *view = [self.mapView rendererForOverlay:circle];
-        if (view)
-        {
-            
-            CGPoint point = [recognizer locationInView:self.mapView];
-            
-            
-//            CGRect viewFrameInMapView = [view.superview convertRect:view.frame toView:mapView];
-//            // Get touch point in the mapView's coordinate system
-//            CGPoint point = [recognizer locationInView:mapView];
-//            // Check if the touch is within the view bounds
-            
-            
-            
-            
-//            CGRect viewFrameInMapView = [view.superview convertRect:view.frame toView:mapView];
-//            CGPoint point = [recognizer locationInView:self.mapView];
-//            if (CGRectContainsPoint(viewFrameInMapView, point))
-//            {
-//                tappedOverlay = circle;
-//                break;
-//            }
-        }
-    }
-//    NSLog(@"Tapped view: %@", [mapView viewForOverlay:tappedOverlay]);
-}
-
-// next steps:
-
-// CIRCLES ARE TAPABLE
-
-// WHEN A CIRCLE IS TAPPED, A SMALL VIEW FADES IN THAT DISPLAYS THE SIGN'S REGULATION TEXT
-// CLOSE THE SMALL VIEW BY TAPPING ANYWHERE
-
-//-(BOOL)displayCircle:(FISSign *)streetSign
-//{
-//    NSUInteger signHourStarts = streetSign.hourStarts;
-//    NSUInteger signHourEnds = streetSign.hourEnds;
-//    if (()) {
-//        <#statements#>
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*****************************
- *      ADDING A CIRCLE      *
- *****************************/
-
-
-//- (void)addSignLocation
-//{
-//    FISCircle *aCircle = (FISCircle *)[FISCircle circleWithCenterCoordinate:CLLocationCoordinate2DMake(40.707721, -74.012952) radius:9];
-//    aCircle.color = [UIColor colorWithRed:0 green:255 blue:213 alpha:0.8];
-//    [self.mapView addOverlay:aCircle]; // adding sign to the map as overlay
-//}
-//
-//
-//
-//- (MKCircleRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay
-//{
-//    if ([overlay isKindOfClass:FISCircle.class]) {
-//        
-//        FISCircle *circle = (FISCircle *)overlay;
-//        MKCircleRenderer *circleView = [[MKCircleRenderer alloc] initWithOverlay:overlay];
-//        circleView.fillColor = circle.color;
-//        
-//        return circleView;
-//    }
-//    
-//    return nil;
-//}
-
-
-
-
-
-
-
-
-
 
 @end
-
-
-
-
